f46b50b834453b8be6d5750f329ba22b39a886c4
İrem Akın Tunga <iremakin00@gmail.com>
2023-11-01 20:31:22 +0300

    shows yup error msgs

--- kullanici-katilimi/src/components/CreateForm.js
+++ kullanici-katilimi/src/components/CreateForm.js
@@ -1,22 +1,89 @@
-import React, { useState } from "react";
-import { Button } from "react-bootstrap";
-import { Form } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import { Button, Form } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import * as yup from "yup";
-import { userSchema } from "./UserValidation";
+import axios from "axios";
 
-function CreateForm() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [checkbox, setCheckbox] = useState(false);
+const emptyForm = {
+  name: "",
+  email: "",
+  password: "",
+  checkbox: false,
+};
+
+function CreateForm(formData = emptyForm) {
+  const [data, setData] = useState(formData);
+  const { name, email, password, checkbox } = data;
+  const [formValid, setFormValid] = useState(true);
+  const [formErrors, setFormErrors] = useState({
+    name: "",
+    email: "",
+    password: "",
+    checkbox: "",
+  });
+
+  const validationSchema = yup.object().shape({
+    name: yup.string().min(2).required("Name Surname is required!"),
+    email: yup.string().email().required("Email is required"),
+    password: yup
+      .string()
+      .min(4, "Password should include 4-8 characters")
+      .max(8, "Password should include 4-8 characters")
+      .required("Password is requires"),
+    checkbox: yup.boolean().oneOf([true], "Terms must be accepted!"),
+  });
+
+  const onChange = (e) => {
+    const { name, value, type, checked } = e.target;
+    setData({ ...data, [name]: type === "checkbox" ? checked : value });
+    checkValidationFor(name, type === "checkbox" ? checked : value);
+  };
 
   const submitForm = (e) => {
     e.preventDefault();
-    console.log(name, email, password, checkbox);
-  };
+    for (let key in data) {
+      checkValidationFor(key, data[key]);
+    }
 
-  const handleCheck = () => setCheckbox(!checkbox);
+    if (formValid) {
+      console.log("Form Submitted! ", e);
+
+      const endpoint = "https://reqres.in/api/users";
+
+      axios
+        .post(endpoint, data)
+        .then((res) => {
+          console.log("POST success!");
+        })
+        .catch((err) => {
+          console.error("POST error!", err);
+        });
+    }
+  };
+  const checkValidationFor = (field, value) => {
+    console.log("Validating field:", field, "Value:", value);
+
+    yup
+      .reach(validationSchema, field)
+      .validate(value)
+      .then(() => {
+        setFormErrors((prevFormErrors) => ({
+          ...prevFormErrors,
+          [field]: "",
+        }));
+      })
+      .catch((err) => {
+        console.log("Validation error for", field, ":", err.errors[0]);
+        setFormErrors((prevFormErrors) => ({
+          ...prevFormErrors,
+          [field]: err.errors[0],
+        }));
+      });
+  };
+  useEffect(() => {
+    validationSchema.isValid(data).then((valid) => setFormValid(valid));
+  }, [data]);
+
   return (
     <>
       <h3>Registration Form</h3>
@@ -26,43 +93,63 @@ function CreateForm() {
           <Form.Label> Name Surname </Form.Label>
           <Form.Control
             required
+            name="name"
             type="text"
-            placeholder="name surname"
             value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
+            placeholder="name surname"
+            onChange={onChange}
+            isInvalid={!!formErrors.name}
+          />{" "}
+          <Form.Control.Feedback type="invalid">
+            {formErrors.name}
+          </Form.Control.Feedback>
         </Form.Group>
 
         <Form.Group className="mb-3">
           <Form.Label> Enter e-mail </Form.Label>
           <Form.Control
             required
+            name="email"
             type="email"
             placeholder="example@example.com"
             value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            onChange={onChange}
+            isInvalid={!!formErrors.email}
           />
+          <Form.Control.Feedback type="invalid">
+            {formErrors.email}
+          </Form.Control.Feedback>
         </Form.Group>
 
         <Form.Group className="mb-3">
           <Form.Label> Password </Form.Label>
           <Form.Control
             required
+            name="password"
             type="password"
             placeholder="password"
             value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
+            onChange={onChange}
+            isInvalid={!!formErrors.password}
+          />{" "}
+          <Form.Control.Feedback type="invalid">
+            {formErrors.password}
+          </Form.Control.Feedback>
         </Form.Group>
 
         <Form.Group className="mb-3">
           <Form.Label> Terms Of Service </Form.Label>
           <Form.Check
             required
+            name="checkbox"
             type="checkbox"
             checked={checkbox}
-            onClick={handleCheck}
-          />
+            onChange={onChange}
+            isInvalid={!!formErrors.checkbox}
+          />{" "}
+          <Form.Control.Feedback type="invalid">
+            {formErrors.checkbox}
+          </Form.Control.Feedback>
         </Form.Group>
 
         <Button variant="primary" type="submit">
